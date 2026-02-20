c39796ca23376b0060b7a7908ceb823091946034
nopalppt <naufalshofil1603@gmail.com>
2026-02-20 11:08:12 +0700

    Percobaan 3

---
+++ minggu2/Mahasiswa19.java
@@ -0,0 +1,47 @@
+package minggu2;
+
+public class Mahasiswa19 {
+
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa19(){
+
+    }
+
+    public Mahasiswa19(String nama, String nim, String kelas, double ipk){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    void tampilkanInformasi(){
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+
+    void updateIPK(double ipkBaru){
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja(){
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+}
---
+++ minggu2/MahasiswaMain19.java
@@ -0,0 +1,20 @@
+package minggu2;
+
+public class MahasiswaMain19 {
+    public static void main(String[] args) {
+        Mahasiswa19 mhs1 = new Mahasiswa19();
+        mhs1.nama = "Muahammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI-2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI-2K");
+        mhs1.updateIPK(3.60);
+        mhs1.tampilkanInformasi();
+
+        Mahasiswa19 mhs2 = new Mahasiswa19("Anisa Nabila", "2141720160", "TI-2L", 3.20);
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
+    }
+}
